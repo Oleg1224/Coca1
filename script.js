c4994606a36cf78bfe02f4b2321644e5edb23aeb
Oleg1224 <olegik.kr@gmail.com>
2025-06-01 12:02:19 +0300

    v1

---
+++ script.js
@@ -0,0 +1,28 @@
+// const plusminus = document.querySelectorAll('.block6_plusminus')
+// plusminus.forEach(plusminus=>{
+//     plusminus.addEventListener('click', function() {
+//     if(plusminus.src.includes('plus.png')) {
+//         plusminus.src='./img/minus.png'
+//     } else {
+//         plusminus.src='./img/plus.png'
+//     }
+//     })
+// })
+// const plusminusIcons = document.querySelectorAll('.block6_plusminus')
+// plusminusIcons.forEach(icon => {
+//     icon.addEventListener('click', function () {
+//         const block = icon.closest('.block6_function')
+//         const title = block.querySelector('.block6_function_title')
+//         const text = block.querySelector('.block6_function_text')
+
+//         if (icon.src.includes('plus.png')) {
+//             icon.src = './img/minus.png'
+//             text.style.display = 'block'
+//             title.classList.add('black')
+//         } else {
+//             icon.src = './img/plus.png'
+//             text.style.display = 'none'
+//             title.classList.remove('black')
+//         }
+//     })
+// })
